f136c6187bc0f50274cf4ea7abea777aad15b2d3
rafat17 <43632236+rafat17@users.noreply.github.com>
2023-04-16 01:50:52 +0600

    2 files modified

--- src/components/LazyLoadImageWithBlurhash.jsx
+++ src/components/LazyLoadImageWithBlurhash.jsx
@@ -2,20 +2,34 @@ import React, { useState, useEffect } from 'react';
 import { BlurhashCanvas } from 'react-blurhash';
 import classnames from 'classnames';
 
+const blurHashCanvasStyles = {
+  width: '100%',
+  height: '100%',
+  borderRadius: '10px',
+};
+
 const LazyLoadImageWithBlurhash = ({ hash, url }) => {
   const [imageHasLoaded, setImageHasLoaded] = useState(false);
+  const [removeBluhashCanvas, setRemoveBlurhashCanvas] = useState(false);
+
+  useEffect(() => {
+    let removeCanvasTimeout;
+
+    if (imageHasLoaded) {
+      removeCanvasTimeout = setTimeout(() => {
+        setRemoveBlurhashCanvas(true);
+      }, 2000);
+    }
+
+    if (removeCanvasTimeout) {
+      clearTimeout(removeCanvasTimeout);
+    }
+  }, [imageHasLoaded]);
 
   return (
     <div className="images-list__container">
-      {!imageHasLoaded && (
-        <BlurhashCanvas
-          hash={hash}
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-          punch={1}
-        />
+      {!removeBluhashCanvas && (
+        <BlurhashCanvas hash={hash} style={blurHashCanvasStyles} punch={1} />
       )}
       <img
         onLoad={() => {
